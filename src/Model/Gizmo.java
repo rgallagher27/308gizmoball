2a5fa6061a5999166b5982ab0f1cdfb31805b8a7
Ross Gallagher <ross_gallagher@rossgallagher.co.uk>
2013-02-06 16:21:38 +0000

    Added in iGizmo Methods and implemented without functionality in Gizmo
    Signed-off-by: Ross Gallagher <ross_gallagher@rossgallagher.co.uk>

--- src/Model/Gizmo.java
+++ src/Model/Gizmo.java
@@ -1,9 +1,60 @@
 package Model;
 
+import java.awt.Point;
+import java.util.List;
+
 public class Gizmo implements iGizmo {
 
 	public Gizmo() {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void doAction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLocation(Point p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Point getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTrigger(iGizmo t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addTrigger(int k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeTrigger(iGizmo t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rotate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<iGizmo> getTriggers() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/Model/iGizmo.java
+++ src/Model/iGizmo.java
@@ -1,5 +1,24 @@
 package Model;
 
+import java.awt.Point;
+import java.util.List;
+
 public interface iGizmo {
+	
+	public void doAction();
+	
+	public void setLocation(Point p);
+	
+	public Point getLocation();
+	
+	public void addTrigger(iGizmo t);
+	
+	public void addTrigger(int k);
+	
+	public void removeTrigger(iGizmo t);
+	
+	public void rotate();
+	
+	public List<iGizmo> getTriggers();
 
 }
