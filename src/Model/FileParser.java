cd25c7c08c42996bec7c573e33a749ebfbafb366
Iain Michael Cuthbertson <iciuthers@gmail.com>
2013-02-13 00:16:41 +0000

    Load from file should be working just need to add code to create create all the gizmos and such

--- src/Model/FileParser.java
+++ src/Model/FileParser.java
@@ -1,4 +1,11 @@
-pachage Model;
+package Model;
+
+import java.io.FileInputStream;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import java.util.ArrayList;
 
 public class FileParser {
 
@@ -6,11 +13,69 @@ public class FileParser {
         
     }
     
-    public loadFile() {
+    public void loadFile(String fileName) {
+        ArrayList<String> input = new ArrayList<String>();
+        String strLine = "";
         
+        try {
+            FileInputStream fstream = new FileInputStream(fileName);
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            
+            while((strLine = br.readLine()) != null) {
+                input.add(strLine);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        generate(input);
     }
     
-    public saveFile() {
+    private void generate(ArrayList<String> input) {
+        for(String s : input) {
+            String[] tok = s.split("\\s");
+            
+            switch(tok[0]) {
+                case "":  //blank line in file
+                    break;
+                case "Absorber":  //create Absorber
+                    break;
+                case "Ball":  //create Ball
+                    break;
+                case "Bumper":  //create Bumper
+                    break;
+                case "Circle":  //create Circle
+                    break;
+                case "LeftFlipper":  //create LeftFlipper
+                    break;
+                case "RightFlipper":  //create RightFlipper
+                    break;
+                case "Square":  //create Square
+                    break;
+                case "Triangle":  //create Triangle
+                    break;
+                case "Wall":  //create Wall
+                    break;
+                case "Gravity": //set Gravity
+                    break;
+                case "Friction": //set Friction
+                    break;
+                case "Move": //move Gizmo
+                    break;
+                case "Rotate": //rotate Gizmo
+                    break;
+                case "Connect": //connect Gizmos
+                    break;
+                case "KeyConnect": //key connect Gizmo
+                    break;
+                default: //Error throw up error message dialog box
+                    break;
+            }
+        }
+    }
+    
+    public void saveFile() {
         
     }
 
