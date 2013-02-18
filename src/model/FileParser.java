d39db555b38baf2c552c1b5cdd44df82702b2bac
Iain Michael Cuthbertson <iciuthers@gmail.com>
2013-02-18 02:24:10 +0000

    first version of load file to show how it'll work i'll add the rest of them later

--- src/model/FileParser.java
+++ src/model/FileParser.java
@@ -6,8 +6,11 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FileParser {
+    
+    private ArrayList<String[]> absorbers = new ArrayList<String[]>();
 
     public FileParser() {
         
@@ -39,7 +42,8 @@ public class FileParser {
             switch(tok[0]) {
                 case "":  //blank line in file
                     break;
-                case "Absorber":  //create Absorber
+                case "Absorber":
+                    absorbers.add(Arrays.copyOfRange(tok, 1, tok.length));
                     break;
                 case "Ball":  //create Ball
                     break;
@@ -77,6 +81,10 @@ public class FileParser {
     
     public void saveFile() {
         
+    }
+    
+    public ArrayList<String[]> getAbsorbers() {
+        return absorbers;
     }
 
 }
