0c0e317738035c306f46bc667276f7e5abaeb337
fedexer <arran.balsillie@strath.ac.uk>
2013-02-21 01:55:56 +0000

    Added the Ball class, still empty, needs fleshed out.

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -23,66 +23,6 @@ public class Ball implements iBall {
 	}
 
 	@Override
-	public void doAction() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setLocation(Point p) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Point getLocation() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Point getBounds() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addTrigger(iGizmo t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addTrigger(int k) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void removeTrigger(iGizmo t) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void removeTrigger(int k) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public List<iGizmo> getTriggers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void rotate() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void setVelocity(double vx, double vy) {
 		velocityX = vx;
 		velocityY = vy;
@@ -99,18 +39,6 @@ public class Ball implements iBall {
 	public Double getVelocityY() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	@Override
