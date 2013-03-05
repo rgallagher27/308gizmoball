258df20ce3afc84f56cadd5a2a74f7d645b0fdf4
fedexer <arran.balsillie@strath.ac.uk>
2013-03-05 22:32:57 +0000

    IController interface, will be used for all the controllers.

---
+++ src/controller/IController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+
+public interface IController extends KeyListener, ActionListener, MouseListener {
+	
+
+}
