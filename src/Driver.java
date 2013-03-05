4fc8d715ab89b11b4f8001de60e657f89f29f0e5
fedexer <arran.balsillie@strath.ac.uk>
2013-03-05 22:29:55 +0000

    Driver now sets up the MVC correctly.

--- src/Driver.java
+++ src/Driver.java
@@ -3,20 +3,26 @@ import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+import controller.AnimationEventListener;
+import controller.IController;
+
+import model.Overlord;
+import model.iOverlord;
+
+import view.CompleteViewContainer;
 import view.PrototypeView;
 
 public class Driver {
 
 	public Driver() {
-		JFrame prototypeFrame = new JFrame();
-		prototypeFrame.add(new PrototypeView());
 		
-		prototypeFrame.setBackground(Color.black);
-		
-		prototypeFrame.pack();
-		prototypeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		prototypeFrame.setVisible(true);
+		CompleteViewContainer cvc = new CompleteViewContainer();
+		iOverlord ov = new Overlord(cvc.getPlayView().getGridSize(), cvc.getPlayView().getCanvasSize());
+		((Overlord) ov).addObserver(cvc.getPlayView());
+		IController controller = new AnimationEventListener(ov);
+		cvc.addController(controller);
 	}
+	
 	
 	public static void main(String[] args)
 	{	
