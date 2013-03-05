a28f49c78ea70bfd16b457d04be0ef5c2323e816
fedexer <arran.balsillie@strath.ac.uk>
2013-03-05 23:31:07 +0000

    Ball/Gizmo point classes.

---
+++ src/model/GizPoint.java
@@ -0,0 +1,28 @@
+package model;
+
+public class GizPoint {
+
+	private int x;
+	private int y;
+	
+	public GizPoint(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
+}
