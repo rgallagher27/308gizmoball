9789666d77cb172fd8b95f58ce389dd449274b64
Iain Michael Cuthbertson <iciuthers@gmail.com>
2013-03-20 01:19:36 +0000

    added panels and buttons, going to try add in the listeners next

--- src/view/CompleteViewContainer.java
+++ src/view/CompleteViewContainer.java
@@ -1,6 +1,6 @@
 package view;
 
-import java.awt.Color;
+import java.awt.*;
 
 import javax.swing.*;
 
@@ -15,6 +15,26 @@ public class CompleteViewContainer extends JFrame {
 	private boolean mode;
 	private JMenuBar menuBar;
 	private JMenu menu;
+	
+	private JRadioButton square;
+	private JRadioButton triangle;
+	private JRadioButton leftFlipper;
+	private JRadioButton rightFlipper;
+	private JRadioButton absorber;
+	private JRadioButton remove;
+	private JRadioButton rotate;
+	private JRadioButton addTrigger;
+	private JRadioButton removeTrigger;
+
+	private JPanel canvasPanel;
+	private JPanel playPanel;
+	private JPanel buildPanel;
+
+	private JButton addBall;
+	private JButton load;
+	private JButton save;
+	private JButton play;
+
 	private GUIController guiListener;
 	
 	
@@ -51,9 +71,48 @@ public class CompleteViewContainer extends JFrame {
 		menuBar.add(menu);
 		setJMenuBar(menuBar);
 		/* menu bar setup */
+
+		absorber = new JRadioButton("Add Absorber");
+		square = new JRadioButton("Add Square");
+		triangle = new JRadioButton("Add Triangle");
+		leftFlipper = new JRadioButton("Add Left Flipper");
+		rightFlipper = new JRadioButton("Add Right Flipper");
+		remove = new JRadioButton("Remove Gizmo");
+		rotate = new JRadioButton("Rotate Gizmo");
+		addTrigger = new JRadioButton("Add Trigger");
+		removeTrigger = new JRadioButton("Remove Trigger");
+
+		addBall = new JButton("Add Button");
+		load = new JButton("Load");
+		save = new JButton("Save");
+		play = new JButton("Play");
+		
+		canvasPanel = new JPanel();
+		buildPanel = new JPanel();
+		buildPanel.setLayout(new GridLayout(10,1));
+		playPanel = new JPanel();
+		playPanel.setLayout(new GridLayout(3,1));
 		
 		/* setup content area */
-		getContentPane().add(playView);
+		canvasPanel.add(playView);
+		buildPanel.add(absorber);
+		buildPanel.add(square);
+		buildPanel.add(triangle);
+		buildPanel.add(leftFlipper);
+		buildPanel.add(rightFlipper);
+		buildPanel.add(remove);
+		buildPanel.add(rotate);
+		buildPanel.add(addTrigger);
+		buildPanel.add(removeTrigger);
+		buildPanel.add(addBall);
+
+		playPanel.add(load);
+		playPanel.add(save);
+		playPanel.add(play);
+
+		getContentPane().add(canvasPanel);
+		getContentPane().add(playPanel, BorderLayout.EAST);
+		
 		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
